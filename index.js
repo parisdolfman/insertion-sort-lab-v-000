641067dd04d4a64e90cee722a970f11f16f8b226
Paris Dolfman <parisdolfman@gmail.com>
2019-03-25 17:43:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function findMinAndRemove(array){
-
+ let min = array[0]
+ let minIndex = 0
+ for (let counter = 0, counter < array.length; counter++) {
+   let currentElement = array[counter]
+   if (array[counter] < min) {
+     min = array[counter]
+     min index = counter
+   }
+ }
 }
 
 function insertionSort(array){
