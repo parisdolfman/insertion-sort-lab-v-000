b8bc45f5e92c67a197678af377cff5258618e8ea
Paris Dolfman <parisdolfman@gmail.com>
2019-03-25 17:45:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 function findMinAndRemove(array){
  let min = array[0]
  let minIndex = 0
-
  for (let counter = 0; counter < array.length; counter++) {
-
    let currentElement = array[counter]
    if (array[counter] < min) {
      min = array[counter]
